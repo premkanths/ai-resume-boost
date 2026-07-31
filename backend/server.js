b7ade2dc3d3f8a11f4d1cae2e8e9e95f7513083c
Premkanth KS <premkanthks@gmail.com>
2026-07-31 22:27:33 +0530

    fix: bind server to port and 0.0.0.0 for Render

--- backend/server.js
+++ backend/server.js
@@ -31,11 +31,8 @@ app.get("/", (req, res) => {
 });
 
 // Server start
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
-/* app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-}); */
-app.listen(PORT, "127.0.0.1", () => {
-  console.log(`Server running on http://127.0.0.1:${PORT}`);
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
